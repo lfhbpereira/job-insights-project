11f5ca8e6a7e5b5158527c1c52df778485f4a6b5
Luiz Pereira <lfhbpereira@gmail.com>
2023-03-28 12:14:01 -0300

    feat(req02): add get_unique_job_types function in jobs

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -11,21 +11,9 @@ def read(path: str) -> List[Dict]:
 
 
 def get_unique_job_types(path: str) -> List[str]:
-    """Checks all different job types and returns a list of them
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique job types
-    """
-    raise NotImplementedError
+    jobs = read(path)
+    job_types = {job["job_type"] for job in jobs}
+    return list(job_types)
 
 
 def filter_by_job_type(jobs: List[Dict], job_type: str) -> List[Dict]:
