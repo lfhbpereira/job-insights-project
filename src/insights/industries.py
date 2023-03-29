1ac14acebb4d805257e2524c5552581935fc2996
Luiz Pereira <lfhbpereira@gmail.com>
2023-03-29 12:30:07 -0300

    refactor(req07): rename variable

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -11,6 +11,6 @@ def get_unique_industries(path: str) -> List[str]:
 
 
 def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:
-    industry = [job for job in jobs if job["industry"] == industry]
+    jobs_by_industry = [job for job in jobs if job["industry"] == industry]
 
-    return industry
+    return jobs_by_industry
