04414e7b5188648fdea8256dd011d7d5f7fa8c40
Luiz Pereira <lfhbpereira@gmail.com>
2023-03-31 23:30:25 -0300

    refactor(req10/11): update location of mock files

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -2,7 +2,7 @@ from src.pre_built.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    result = read_brazilian_file("tests/mocks/brazilians_jobs.csv")
+    result = read_brazilian_file("tests/brazilian/mock/brazilians_jobs.csv")
     assert result[0] == {
         "title": "Maquinista",
         "salary": "2000",
--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -2,4 +2,4 @@ from src.pre_built.counter import count_ocurrences
 
 
 def test_counter():
-    assert count_ocurrences("tests/mocks/jobs.csv", "full") == 3
+    assert count_ocurrences("tests/counter/mock/jobs.csv", "full") == 3
